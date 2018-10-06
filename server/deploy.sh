65f69c83205535fe740e05f968076a8d9b571e72
sadainiqrar <sadain.abbasi@gmail.com>
2018-10-06 23:43:20 +0500

    config staging

--- server/deploy.sh
+++ server/deploy.sh
@@ -4,7 +4,10 @@ echo "1/7) Compile sources ..."
 
 NODE_ENV=production webpack --config webpack.server.config.js --progress
 
+echo "2/7) Stopping existing node process ..."
+forever stop bincee
+
 echo "3/7) Starting node process ..."
-NODE_ENV=production CONFIG=production node server/dist/build.js .
+NODE_ENV=production CONFIG=production forever start --uid "bincee" --append -c "node server/dist/build.js" .
 
 echo "Deployment complete"
