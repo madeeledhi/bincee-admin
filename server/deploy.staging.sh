097aa9f07dd704887178a96f1df2b95106dc2235
sadainiqrar <sadain.abbasi@gmail.com>
2018-10-07 00:48:05 +0500

    config staging

--- server/deploy.staging.sh
+++ server/deploy.staging.sh
@@ -1,9 +1,12 @@
 #!/bin/bash -x
 
-echo "1/3) Compile sources ..."
+echo "1/7) Compile sources ..."
 NODE_ENV=production webpack --config webpack.server.config.js --progress
 
-echo "2/3) Starting node process ..."
-NODE_ENV=production CONFIG=staging node server/dist/build.js
+echo "2/7) Stopping existing node process ..."
+forever stop vizdev
+
+echo "3/7) Starting node process ..."
+NODE_ENV=production CONFIG=staging forever start --uid "vizdev" --append -c "node server/dist/build.js" .
 
 echo "Deployment complete"
