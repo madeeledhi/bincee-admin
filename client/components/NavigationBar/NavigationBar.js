5f0e6f8bb45c24dafa4ed663f59a14c1f60e1d10
Muhammad Usman <muhammadusman1794@gmail.com>
2018-11-24 01:16:31 +0500

    navigation bar intiall structure

---
+++ client/components/NavigationBar/NavigationBar.js
@@ -0,0 +1,32 @@
+// lib
+import React from 'react'
+import List from '@material-ui/core/List'
+import Listitem from '@material-ui/core/Listitem'
+import ListItemText from '@material-ui/core/ListItemText'
+import ListSubheader from '@material-ui/core/ListSubheader'
+
+// src
+
+const NavigationBar = props => {
+  const { onRouteChange } = props
+
+  return (
+    <div>
+      <List>
+        <Listitem button onClick={() => onRouteChange('dashboard/home')}>
+          <ListItemText primary={'Home'} />
+        </Listitem>
+        <Listitem button onClick={() => onRouteChange('dashboard/student')}>
+          <ListItemText primary={'Student'} />
+        </Listitem>
+        <Listitem button onClick={() => onRouteChange('dashboard/home')}>
+          <ListItemText primary={'Home'} />
+        </Listitem>
+        <Listitem button onClick={() => onRouteChange('dashboard/student')}>
+          <ListItemText primary={'Student'} />
+        </Listitem>
+      </List>
+    </div>
+  )
+}
+export default NavigationBar
--- client/components/dashboard/DashboardInner.js
+++ client/components/dashboard/DashboardInner.js
@@ -7,6 +7,8 @@ import { Redirect, Route, Switch } from 'react-router'
 import Header from '../Header'
 import Home from '../Home'
 import Student from '../Student'
+import NavigationBar from '../NavigationBar'
+import styles from './Dashboard.less'
 
 const DashboardInner = ({
   onClickSignout,
@@ -15,18 +17,23 @@ const DashboardInner = ({
   path,
   onRouteChange,
 }) => (
-  <div>
-    <Header
-      onClickSignout={onClickSignout}
-      user={user}
-      authenticated={authenticated}
-      onRouteChange={onRouteChange}
-    />
-    <Switch>
-      <Route path={path} exact component={Home} />
-      <Route path={`${path}/student`} exact component={Student} />
-      <Redirect to="/dashboard" />
-    </Switch>
+  <div className={styles.root}>
+    <div className={styles.navigation}>
+      <NavigationBar onRouteChange={onRouteChange} />
+    </div>
+    <div className={styles.head}>
+      <Header
+        onClickSignout={onClickSignout}
+        user={user}
+        authenticated={authenticated}
+        onRouteChange={onRouteChange}
+      />
+      <Switch>
+        <Route path={path} exact component={Home} />
+        <Route path={`${path}/student`} exact component={Student} />
+        <Redirect to="/dashboard" />
+      </Switch>
+    </div>
   </div>
 )
 
