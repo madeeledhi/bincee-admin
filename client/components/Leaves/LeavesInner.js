0aaa5063f47c0f90b450fda1916fd945b6c252af
Muhammad Usman <muhammadusman1794@gmail.com>
2018-12-21 00:44:08 +0500

    leaves styling demo

--- client/components/Leaves/LeavesInner.js
+++ client/components/Leaves/LeavesInner.js
@@ -3,16 +3,29 @@ import React from 'react'
 import getOr from 'lodash/fp/getOr'
 import size from 'lodash/fp/size'
 import MaterialTable from 'material-table'
-
+import MTableToolbar from 'material-table/dist/m-table-toolbar'
 // src
 import EnhancedTable from '../EnhancedTable'
 import LoadingView from '../LoadingView'
+import styles from './LeavesInner.less'
 
 const LeavesInner = ({ error, isLoading, rows, data }) => (
   <Choose>
     <When condition={!error && !isLoading}>
       <div>
-        <MaterialTable title={'Leaves'} columns={rows} data={data} />
+        <MaterialTable
+          components={{
+            Toolbar: props => (
+              <MTableToolbar
+                classes={{ root: styles.root, title: styles.head }}
+                title={'Leaves'}
+                {...props}
+              />
+            ),
+          }}
+          columns={rows}
+          data={data}
+        />
       </div>
     </When>
     <Otherwise>
