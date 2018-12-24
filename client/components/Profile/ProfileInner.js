028f48b25c66d091f49d88fd4f4e44af9fc52118
Zuaraiz <zuraiz.muhammad123@gmail.com>
2018-12-24 21:28:45 +0500

    profile page settings

--- client/components/Profile/ProfileInner.js
+++ client/components/Profile/ProfileInner.js
@@ -18,63 +18,73 @@ const ProfileInner = props => {
   } = props
   return (
     <form className={styles.root}>
-      <div className={styles.row}>
-        <Field
-          id="name"
-          name="name"
-          component={renderTextField}
-          label="Name"
-          disabled={false}
-          variant="outlined"
-          className={styles.item}
-        />
+      <div className={styles.fieldPosition} >
+        <div className={styles.row}>
+          <div className={styles.spacing} />
+          <Field
+            id="name"
+            name="name"
+            component={renderTextField}
+            label="Name"
+            disabled={false}
+            variant="outlined"
+            className={styles.item}
+          />
+        </div>
+        <div className={styles.row}>
+          <div className={styles.spacing} />
+          <Field
+            id="phone_no"
+            name="phone_no"
+            component={renderTextField}
+            label="Phone No"
+            disabled={false}
+            variant="outlined"
+            className={styles.item}
+          />
+        </div>
+        <div className={styles.row}>
+          <div className={styles.spacing} />
+          <Field
+            id="email"
+            name="email"
+            component={renderTextField}
+            label="Email"
+            disabled={false}
+            variant="outlined"
+            className={styles.item}
+          />
+        </div>
+        <div className={styles.row}>
+          <div className={styles.spacing} />
+          <Field
+            id="address"
+            name="address"
+            component={renderTextField}
+            label="Address"
+            disabled={false}
+            variant="outlined"
+            className={styles.item}
+          />
+        </div>
       </div>
-      <div className={styles.row}>
-        <Field
-          id="phone_no"
-          name="phone_no"
-          component={renderTextField}
-          label="Phone No"
-          disabled={false}
-          variant="outlined"
-          className={styles.item}
-        />
+      <div className={styles.mapPlace}>
+        <div className={styles.row}>
+          <MapPicker
+            height={500}
+            width={500}
+            defaultPosition={defaultPosition}
+            zoom={16}
+            onChange={val => {
+              handlePositionChange(val)
+            }}
+          />
+        </div>
       </div>
+
       <div className={styles.row}>
-        <Field
-          id="email"
-          name="email"
-          component={renderTextField}
-          label="Email"
-          disabled={false}
-          variant="outlined"
-          className={styles.item}
-        />
-      </div>
-      <div className={styles.row}>
-        <Field
-          id="address"
-          name="address"
-          component={renderTextField}
-          label="Address"
-          disabled={false}
-          variant="outlined"
-          className={styles.item}
-        />
-      </div>
-      <div className={styles.row}>
-        <MapPicker
-          height={500}
-          width={500}
-          defaultPosition={defaultPosition}
-          zoom={16}
-          onChange={val => {
-            handlePositionChange(val)
-          }}
-        />
-      </div>
-      <div className={styles.row}>
-        <div className={styles.item}>
+        <div className={styles.button}>
+          <div className={styles.spacing} />
           <Button
             disabled={disabled}
             onClick={handleUpdate}
