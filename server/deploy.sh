06803ddbe2996016f52a2edbade12cad4ed95904
sadainiqrar <sadain.abbasi@gmail.com>
2019-01-05 02:50:04 +0500

    kill command

--- server/deploy.sh
+++ server/deploy.sh
@@ -8,6 +8,7 @@ echo "2/7) Stopping existing node process ..."
 forever stop bincee
 
 echo "3/7) Starting node process ..."
+sudo lsof -t -i tcp:4000 -s tcp:listen | sudo xargs kill
 NODE_ENV=production CONFIG=production forever start --uid "bincee" --append -c "node server/dist/build.js" .
 
 echo "Deployment complete"
