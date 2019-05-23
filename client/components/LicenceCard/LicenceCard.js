b32281be64995b964089cca94158ee9bfb697a8e
Zuaraiz <zuraiz.muhammad123@gmail.com>
2019-05-23 05:55:34 +0500

    Email and time cards initial

--- client/components/LicenceCard/LicenceCard.js
+++ client/components/LicenceCard/LicenceCard.js
@@ -11,45 +11,106 @@ import styles from './LicenceCard.less'
 const LicenceCard = ({ details }) => {
   const { total, active, inactive, title } = details
   return (
-    <Card className={styles.card}>
-      <CardHeader
-        classes={{ title: styles.cardHeaderText }}
-        avatar={
-          <img
-            src={`/images/${title === 'App Licences' ? 'copy' : 'bus'}.png`}
-          />
-        }
-        title={title}
-        className={styles.cardHeader}
-      />
 
-      <CardContent className={styles.cardContent}>
-        <div className={styles.divider} />
-        <div className={styles.cardContentdiv}>
-          <CardItem
-            label="Total"
-            value={total}
-            unit="Licences"
-            backgroudColor="#eafaff"
-            color="#06b9ed"
+    <Choose>
+      <When condition={title === 'Email'}>
+        <Card className={styles.emailCard}>
+          <CardHeader
+            classes={{ title: styles.cardHeaderText }}
+            avatar={
+              <img
+                src={`/images/${title === 'App Licences' ? 'copy' : 'bus'}.png`}
+              />
+            }
+            title={title}
+            className={styles.cardHeader}
           />
-          <CardItem
-            label="Active"
-            value={active}
-            unit="Licences"
-            backgroudColor="#b8ffe4"
-            color="#0fde8e"
+          <CardContent className={styles.cardContent}>
+            <div className={styles.divider} />
+            <div className={styles.email}>
+              Feature Not Available
+            </div>
+          </CardContent>
+        </Card>
+      </When>
+      <When condition={title === 'Time'}>
+        <Card className={styles.timeCard}>
+          <CardHeader
+            classes={{ title: styles.cardHeaderText }}
+            avatar={
+              <img
+                src={`/images/${title === 'App Licences' ? 'copy' : 'bus'}.png`}
+              />
+            }
+            title={title}
+            className={styles.cardHeader}
           />
-          <CardItem
-            label="Inactive"
-            value={inactive}
-            unit="Licences"
-            backgroudColor="#ffe1e5"
-            color="#ff4560"
+          <CardContent className={styles.cardContent}>
+            <div className={styles.divider} />
+            <div className={styles.cardContentdiv}>
+              {/* <div>Your trial version ends in</div> */}
+              <div className={styles.clockItem}>
+                01
+              </div>
+              <div className={styles.clockItem}>
+                01
+              </div>
+              <div className={styles.collon}>
+                :
+              </div>
+              <div className={styles.clockItem}>
+                01
+              </div>
+              <div className={styles.clockItem}>
+                01
+              </div>
+            </div>
+          </CardContent>
+        </Card>
+      </When>
+      <Otherwise>
+        <Card className={styles.card}>
+          <CardHeader
+            classes={{ title: styles.cardHeaderText }}
+            avatar={
+              <img
+                src={`/images/${title === 'App Licences' ? 'copy' : 'bus'}.png`}
+              />
+            }
+            title={title}
+            className={styles.cardHeader}
           />
-        </div>
-      </CardContent>
-    </Card>
+          <CardContent className={styles.cardContent}>
+            <div className={styles.divider} />
+            <div className={styles.cardContentdiv}>
+              <CardItem
+                label="Total"
+                value={total}
+                unit="Licences"
+                backgroudColor="#eafaff"
+                color="#06b9ed"
+              />
+              <CardItem
+                label="Active"
+                value={active}
+                unit="Licences"
+                backgroudColor="#b8ffe4"
+                color="#0fde8e"
+              />
+              <CardItem
+                label="Inactive"
+                value={inactive}
+                unit="Licences"
+                backgroudColor="#ffe1e5"
+                color="#ff4560"
+              />
+            </div>
+          </CardContent>
+        </Card>
+      </Otherwise>
+    </Choose>
+
+
   )
 }
 export default LicenceCard
--- client/components/Licences/LicencesInner.js
+++ client/components/Licences/LicencesInner.js
@@ -5,12 +5,21 @@ import map from 'lodash/fp/map'
 // src
 import LicenceCard from '../LicenceCard'
 import styles from './LicencesInner.less'
-
+const emailobj= {'title': 'Email'},
+  time = {'title': 'Time'}
 const LicencesInner = ({ data }) => {
   const { licenceInfo, trialInfo } = data
+  console.log(licenceInfo)
   return (
-    <div className={styles.root}>
-      {map(obj => <LicenceCard details={obj} />)(licenceInfo)}
+    <div>
+      
+      <div className={styles.root}>
+        {map(obj => <LicenceCard details={obj} />)(licenceInfo)}
+      </div>
+      <div className={styles.root}>
+        <LicenceCard details={emailobj} />
+        <LicenceCard details={time} />
+      </div>
     </div>
     // TODO: add Trial Card Here
   )
